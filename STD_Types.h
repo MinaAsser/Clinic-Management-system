665879ee215f696b9dda86d7cbfe636820f16e02
MinaAsser <107791790+MinaAsser@users.noreply.github.com>
2022-09-14 19:53:31 +0200

    Add files via upload

---
+++ STD_Types.h
@@ -0,0 +1,15 @@
+#ifndef STD_Types_H
+#define STD_Types_H
+
+
+
+typedef char s8;
+typedef unsigned char u8;
+typedef int s32;
+typedef unsigned int u32;
+typedef short int s16;
+typedef unsigned short int u16;
+typedef  float f32;
+typedef double f64;
+typedef long double f80;
+#endif
